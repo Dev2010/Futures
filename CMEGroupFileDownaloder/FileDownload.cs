c1a95f8d61fbe486f7635f0412e4b9f4fbedfee6
Nish <Nish>
2020-12-31 17:10:33 -0500

    cme group position limit file downloader

---
+++ CMEGroupFileDownaloder/FileDownload.cs
@@ -0,0 +1,68 @@
+﻿using log4net;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace CMEGroupFileDownaloder
+{
+    class FileDownload
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            try
+            {
+                Logger.Info(LogHelper.LogInfo(MethodBase.GetCurrentMethod()));
+                FileDownload fileDownload = new FileDownload();
+                fileDownload.Download(Config.Default.CMEPositionLimitFileURI, Config.Default.Destination);
+                fileDownload.Download(Config.Default.CBOTPositionLimitFileURI, Config.Default.Destination);
+                fileDownload.Download(Config.Default.NYMEXPositionLimitFileURI, Config.Default.Destination);
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception);
+            }
+        }
+
+        private void Download(string uri, string destination)
+        {
+            Logger.Info(LogHelper.LogInfo(MethodBase.GetCurrentMethod(), uri, destination));
+
+            try
+            {
+                string fileName = System.IO.Path.GetFileName(uri);
+
+                Logger.Info(fileName);
+
+                if (!Directory.Exists(destination))
+                {
+                    Directory.CreateDirectory(destination);
+                }
+
+                string combined = Path.Combine(destination, fileName);
+
+                Logger.Info(combined);
+
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(uri, combined);
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception);                
+            }
+        }
+    }
+}
---
+++ Utils/LogHelper.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class LogHelper
+    {
+        public static string StackTraceToString(StackTrace stackTrace)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var frame = stackTrace.GetFrame(0);
+            int line = frame.GetFileLineNumber();
+            stringBuilder.AppendLine(stackTrace.ToString());
+            stringBuilder.AppendLine("Line:" + line);
+            stringBuilder.AppendLine(StackFramesToString(new StackFrame[] { frame }));
+            return stringBuilder.ToString();
+        }
+        public static string LogInfo(MethodBase method, params object[] values)
+        {
+            ParameterInfo[] parms = method.GetParameters();
+            object[] namevalues = new object[2 * parms.Length];
+
+            string msg = method.Name + "(";
+            for (int i = 0, j = 0; i < parms.Length; i++, j += 2)
+            {
+                msg += "{" + j + "}={" + (j + 1) + "}, ";
+                namevalues[j] = parms[i].Name;
+                if (i < values.Length) namevalues[j + 1] = values[i];
+            }
+            msg += ")";
+
+            return string.Format(msg, namevalues);
+        }
+
+        public static string LogError(MethodBase method, Exception ex, params object[] values)
+        {
+            ParameterInfo[] parms = method.GetParameters();
+            object[] namevalues = new object[2 * parms.Length];
+
+            string msg = "Error in " + method.Name + "(";
+            for (int i = 0, j = 0; i < parms.Length; i++, j += 2)
+            {
+                msg += "{" + j + "}={" + (j + 1) + "}, ";
+                namevalues[j] = parms[i].Name;
+                if (i < values.Length) namevalues[j + 1] = values[i];
+            }
+            msg += "exception=" + ex.Message + ")";
+
+            return string.Format(msg, namevalues);
+        }
+
+        public static string StackFramesToString(this StackFrame[] frames, int numMethodsToSkip = 0)
+        {
+            //bool displayFilenames = true;   // we'll try, but demand may fail
+            String word_At = "at";
+            //String inFileLineNum = "in {0}:line {1}";
+
+            bool fFirstFrame = true;
+            StringBuilder sb = new StringBuilder(255);
+            for (int iFrameIndex = 0; iFrameIndex < frames.Count(); iFrameIndex++)
+            {
+                StackFrame sf = GetFrame(iFrameIndex, frames, numMethodsToSkip);
+                MethodBase mb = sf.GetMethod();
+                if (mb != null)
+                {
+                    // We want a newline at the end of every line except for the last
+                    if (fFirstFrame)
+                        fFirstFrame = false;
+                    else
+                        sb.Append(Environment.NewLine);
+
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "   {0} ", word_At);
+
+                    Type t = mb.DeclaringType;
+                    // if there is a type (non global method) print it
+                    if (t != null)
+                    {
+                        sb.Append(t.FullName.Replace('+', '.'));
+                        sb.Append(".");
+                    }
+                    sb.Append(mb.Name);
+
+                    // deal with the generic portion of the method
+                    if (mb is MethodInfo && ((MethodInfo)mb).IsGenericMethod)
+                    {
+                        Type[] typars = ((MethodInfo)mb).GetGenericArguments();
+                        sb.Append("[");
+                        int k = 0;
+                        bool fFirstTyParam = true;
+                        while (k < typars.Length)
+                        {
+                            if (fFirstTyParam == false)
+                                sb.Append(",");
+                            else
+                                fFirstTyParam = false;
+
+                            sb.Append(typars[k].Name);
+                            k++;
+                        }
+                        sb.Append("]");
+                    }
+
+                    // arguments printing
+                    sb.Append("(");
+                    ParameterInfo[] pi = mb.GetParameters();
+                    bool fFirstParam = true;
+                    for (int j = 0; j < pi.Length; j++)
+                    {
+                        if (fFirstParam == false)
+                            sb.Append(", ");
+                        else
+                            fFirstParam = false;
+
+                        String typeName = "<UnknownType>";
+                        if (pi[j].ParameterType != null)
+                            typeName = pi[j].ParameterType.Name;
+                        sb.Append(typeName + " " + pi[j].Name);
+                    }
+                    sb.Append(")");
+
+                    // source location printing
+                    //if (displayFilenames && (sf.GetILOffset() != -1))
+                    //{
+                    //    // If we don't have a PDB or PDB-reading is disabled for the module,
+                    //    // then the file name will be null.
+                    //    String fileName = null;
+
+                    //    // Getting the filename from a StackFrame is a privileged operation - we won't want
+                    //    // to disclose full path names to arbitrarily untrusted code.  Rather than just omit
+                    //    // this we could probably trim to just the filename so it's still mostly usefull.
+                    //    try
+                    //    {
+                    //        fileName = sf.GetFileName();
+                    //    }
+                    //    catch
+                    //    {
+                    //        // If the demand for displaying filenames fails, then it won't
+                    //        // succeed later in the loop.  Avoid repeated exceptions by not trying again.
+                    //        displayFilenames = false;
+                    //    }
+
+                    //    if (fileName != null)
+                    //    {
+                    //        // tack on " in c:\tmp\MyFile.cs:line 5"
+                    //        sb.Append(' ');
+                    //        sb.AppendFormat(CultureInfo.InvariantCulture, inFileLineNum, fileName, sf.GetFileLineNumber());
+                    //    }
+                    //}
+                }
+            }
+
+            return sb.ToString();
+        }
+
+
+        public static StackFrame GetFrame(int index, StackFrame[] frames, int numMethodsToSkip)
+        {
+            if ((frames != null) && (index < frames.Count()) && (index >= 0))
+                return frames[index + numMethodsToSkip];
+
+            return null;
+        }
+    }
+}
